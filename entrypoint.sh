08e74afcb3d8b9d8a4c74ddf869122563a2bb431
Scott Griffiths <znsgs3@wowcorp.com.au>
2018-10-02 21:25:09 +1000

    updated to use jmeter 5.0 and updated the plugin versions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # moves to workingdirectory to store the large amount of test files for each run
-set -e
 cd ${ENV}
-
 #Runs blazemeter taurus wich executes whats in the config.yaml and points jmeter that was copied across
-bzt ../config.yaml -o modules.jmeter.path=/usr/local/apache-jmeter-4.0.2/bin/jmeter \
+bzt ../config.yaml -o modules.jmeter.path=/usr/local/apache-jmeter-5.0/bin/jmeter \
 -o execution.0.concurrency=${THREADS} \
 -o execution.0.ramp-up=${RAMPUP} \
 -o execution.0.hold-for=${HOLD} \
